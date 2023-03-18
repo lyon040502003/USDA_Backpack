a0de143515653c39a6ba70cdb26bffbf76989155
Lyon Rosenblatt <lyon.home001hap@gmail.com>
2023-03-18 20:55:37 +0100

    usda files can now be written in the right dir with usda files in them and io files in them

--- USDA_Backpack_scripts/Work_scripts/Usda_Pack_main_search.py
+++ USDA_Backpack_scripts/Work_scripts/Usda_Pack_main_search.py
@@ -4,7 +4,7 @@ import shutil
 
 # define master file paths
 file_path = "E:/Tools/USD/File_Sorce/3_layer_usd/layer_1_usda.usda"
-new_file_path = "E:/temp/dell/usd_test2.usda"
+new_file_path = "E:/Tools/USD/OUT_Testusd_test2.usda"
 
 
 # open new master file
@@ -17,10 +17,11 @@ file_name = file_path.split("/")[-1]
 file_drive = file_path.split("/")[-0]
 
 
-# define list off found files in master layer
+# deine paths for the new files to go to 
 sublayer_files = os.path.split(new_file_path)[0].replace('/','\\') + "\\usd_tex"
 sublayer_usda_files = os.path.split(new_file_path)[0].replace('/','\\') + "\\usd_tex\\usda"
 
+#print("file destinatin usda" ,sublayer_usda_files)
 
 # list off all non usd files  
 master_layer_IOfiles_sorce = []
@@ -47,13 +48,14 @@ def copy_files(files_to_copy,file_destination):
 
 # function to recursivle search true all usda files 
 def recurve_usda_search(file):
+
     usd_files = []
     opend_file = open(file, "r")
     # go true all the lines  in the usda file
     for line in opend_file:
         # find all the usda files in the master file
         if "usda@" in line:
-           
+        
             os.chdir(os.path.dirname(os.path.abspath(file)))  
 
             
@@ -61,9 +63,13 @@ def recurve_usda_search(file):
             file_to_copy = os.path.abspath((line.strip().split("@"))[-2]).replace('/','\\') 
             # appen to the sorce and target list
             master_layer_usda_sorce.append(file_to_copy.replace('/','\\'))
+            
+
+
+            
             # array to all files in the found
             usd_files.append(file_to_copy)  
-               
+            
     # recursevly search true all found usda files
     for files in usd_files:
         recurve_usda_search(files)
@@ -72,7 +78,6 @@ def recurve_usda_search(file):
 # function to find all files in all usda files and write new usda files 
 def search_true_usda_files(usda_files): 
     for file in usda_files:
-        
         # function that creates the new usda file and its directory #TODO re build this block its guly af
         if  os.path.exists(os.path.dirname(os.path.abspath(sublayer_usda_files+file.split("\\")[-2]+"\\" +file.split("\\")[-1]))):
             try:
@@ -81,16 +86,19 @@ def search_true_usda_files(usda_files):
                 os.remove(os.path.abspath(sublayer_usda_files+file.split("\\")[-2]+"\\" +file.split("\\")[-1]))
                 new_file = open(os.path.abspath(sublayer_usda_files+file.split("\\")[-2]+"\\" +file.split("\\")[-1]),"x")                
         else:
-            os.mkdir(os.path.dirname(os.path.abspath(sublayer_usda_files+file.split("\\")[-2]+"\\" +file.split("\\")[-1])))
+            os.makedirs(os.path.dirname(os.path.abspath(sublayer_usda_files+file.split("\\")[-2]+"\\" +file.split("\\")[-1])))
             try:
                 new_file = open(os.path.abspath(sublayer_usda_files+file.split("\\")[-2]+"\\" +file.split("\\")[-1]),"x")
             except:
                 os.remove(os.path.abspath(sublayer_usda_files+file.split("\\")[-2]+"\\" +file.split("\\")[-1]))
                 new_file = open(os.path.abspath(sublayer_usda_files+file.split("\\")[-2]+"\\" +file.split("\\")[-1]),"x") 
-        #TODO create function to create new usda file
-   
+
+
         opend_file_old = open(file, "r")
+        new_file_write = open(os.path.abspath(sublayer_usda_files+file.split("\\")[-2]+"\\" +file.split("\\")[-1]),"w")
+        print("curent work file " , os.path.abspath(sublayer_usda_files+file.split("\\")[-2]+"\\" +file.split("\\")[-1]),"w")
         for line in opend_file_old:
+            
             # find all lines that have a file in the line 
             if "file" in line:    
                 
@@ -103,28 +111,34 @@ def search_true_usda_files(usda_files):
                 master_layer_IOfiles_sorce.append(file_to_copy)
                 master_layer_IOfiles_destination.append(file_destination_dir)
 
-                #TODO create function to wirte new relative paths in to the file
+                
                 # generate new line to replace old line in new file 
-                new_rel_file_path = "@." + "/usd_tex/" + file_to_copy.split("\\")[-1] + "@"
-                old_line = "@" + (line.strip().split("@"))[-2] + "@"
-                #file2.write(line.replace(old_line, new_rel_file_path))     
-                #print(old_line)
-                #print(line.replace(old_line, new_rel_file_path))
-                            
-             
-                
-                
-                
-                
-    #       /// CALLS 
+                new_rel_file_path = "@." + "/usd_tex/" + file_to_copy.split("\\")[-2] +"/"+ file_to_copy.split("\\")[-1] + "@"
+                old_rel_path = "@" + (line.strip().split("@"))[-2] + "@"
+
+                # write the new relative path in to the usda file 
+                new_file_write.write(line.replace(old_rel_path, new_rel_file_path))     
+
+            elif "usda@" in line:
+
+                file_to_copy = (os.path.abspath((line.strip().split("@"))[-2]).replace('/','\\')) 
+
+                new_rel_file_path = "@." + "/usd_tex/" + file_to_copy.split("\\")[-2] +"/"+ file_to_copy.split("\\")[-1] + "@"
+                old_rel_path = "@" + (line.strip().split("@"))[-2] + "@"
+
+                print(old_rel_path)
+                new_file_write.write(line.replace(old_rel_path, new_rel_file_path))
+
+            else:
+                new_file_write.write(line)
+
+
+#      /// Calls ///
 
 recurve_usda_search(file_path)
 search_true_usda_files(master_layer_usda_sorce)
 
-
+#print
 
 #      /// TEST PINTS ///
-print("field found usda ")
-print(master_layer_usda_sorce)
-print("files found IO ")
-print(master_layer_IOfiles_sorce)
+
